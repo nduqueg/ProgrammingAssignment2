34fdf68b8f3febb35bede67a5618e02f0c4abae1
nduqueg <nduqueg@unal.edu.co>
2020-08-23 18:15:50 -0500

    the functions were set

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,36 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+# This function creates the special "matrix" that it actually is set from the chache if it already exist
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setmean = setmean,
+       getmean = getmean)
 }
 
 
 ## Write a short comment describing this function
-
+# this function calculates the inverse of a matrix if it has not been calculate and if it was,
+# it set its value to the return variable
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- solve(data, ...)
+  x$setinverse(i)
+  i
+  
+  
+  ## Return a matrix that is the inverse of 'x'
 }
